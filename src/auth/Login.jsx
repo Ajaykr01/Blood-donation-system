68c2a2c6737eec0b59b7d79519768b5979251bea
Ajay <ajaykumar87577@gmail.com>
2025-02-26 16:08:46 +0530

    loginPage completed

---
+++ src/auth/Login.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import Form from "../components/shared/form/form";
+import { useSelector } from "react-redux";
+import Spinner from "../components/shared/Spinner";
+import { toast } from "react-toastify";
+
+const Login = () => {
+  const { loading, error } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+    }
+  }, [error]);
+
+  return (
+    <>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="h-screen flex items-center justify-center bg-black">
+          <div className="w-full max-w-md bg-white rounded-lg shadow-md p-8">
+            <Form
+              formTitle={"Login to Your Account"}
+              submitBtn={"Login"}
+              formType={"loginType"}
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Login;
