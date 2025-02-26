333bae3f19f610fda2bbf0e8572bbf0ef6bad64c
Ajay <ajaykumar87577@gmail.com>
2025-02-26 16:37:14 +0530

    App file  completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,192 @@
-import { Children } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
-import Home from "./components/Home";
-import About from "./components/About";
-import Services from "./components/Services";
-import Contact from "./components/Contact";
-import DonateBlood from "./components/DonateBlood";
-import Login from "./components/Login";
-import Signup from "./components/SignUp";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import DonateBlood from "./pages/Donar/DonateBlood";
+import Login from "./auth/Login";
+import Signup from "./auth/SignUp";
+import { ToastContainer } from "react-toastify";
+import ProtectedRoute from "./components/Routes/ProtectedRoute";
+import PublicRoute from "./components/Routes/PublicRoute";
+import Donar from "./pages/Dashboard/Donar";
+import Hospitals from "./pages/Dashboard/Hospitals";
+import Organisation from "./pages/Dashboard/Organisation";
+import Consumer from "./pages/Dashboard/Consumer";
+import Donation from "./pages/Donation";
+import Analytics from "./pages/Dashboard/Analytics";
+import DonarList from "./pages/Admin/DonarList";
+import HospitalList from "./pages/Admin/HospitalList";
+import AdminHome from "./pages/Admin/AdminHome";
+import HomePage from "./pages/HomePage";
+import DonorDashboard from "./pages/Dashboard/DonarDashboard";
+import DonationHistory from "./pages/Donar/DonationHistory";
 
-function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-        {
-          path: "/about",
-          element: <About />,
-        },
-        {
-          path: "/services",
-          element: <Services />,
-        },
-        {
-          path: "/contact",
-          element: <Contact />,
-        },
-        {
-          path: "/donate",
-          element: <DonateBlood />,
-        },
-        {
-          path: "/login",
-          element: <Login />,
-        },
-        {
-          path:"/signup",
-          element:<Signup />
-        }
-      ],
-    },
-  ]);
+const App = () => {
+  return (
+    <>
+      <ToastContainer />
+      <Routes>
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute>
+              <AdminHome />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/donar-list"
+          element={
+            <ProtectedRoute>
+              <DonarList />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/hospital-list"
+          element={
+            <ProtectedRoute>
+              <HospitalList />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/hospital"
+          element={
+            <ProtectedRoute>
+              <Hospitals />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/analytics"
+          element={
+            <ProtectedRoute>
+              <Analytics />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/consumer"
+          element={
+            <ProtectedRoute>
+              <Consumer />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/donation"
+          element={
+            <ProtectedRoute>
+              <Donation />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/organisation"
+          element={
+            <ProtectedRoute>
+              <Organisation />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/donar"
+          element={
+            <ProtectedRoute>
+              <Donar />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/donar-dashboard"
+          element={
+            <ProtectedRoute>
+              <DonorDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/donate-blood"
+          element={
+            <ProtectedRoute>
+              <DonateBlood />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/donation-history"
+          element={
+            <ProtectedRoute>
+              <DonationHistory />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
 
-  return <RouterProvider router={router} />;
-}
+        <Route path="/" element={<Layout />}>
+          <Route
+            index
+            element={
+              <PublicRoute>
+                <Home />
+              </PublicRoute>
+            }
+          />
+
+          <Route
+            path="/contact"
+            element={
+              <PublicRoute>
+                <Contact />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/services"
+            element={
+              <PublicRoute>
+                <Services />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <PublicRoute>
+                <About />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/signup"
+            element={
+              <PublicRoute>
+                <Signup />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
+        </Route>
+      </Routes>
+    </>
+  );
+};
 
 export default App;
