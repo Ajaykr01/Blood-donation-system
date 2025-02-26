3eaf0a4f9422c592a27026b65f6b2f4fc3ab016b
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2025-02-26 22:42:13 +0530

    update the issue in doner list

--- backend/controllers/donationController.js
+++ backend/controllers/donationController.js
@@ -1,8 +1,8 @@
 const Donation = require("../models/donation");
-
+const mongoose = require('mongoose');
 const donateBlood = async (req, res) => {
   try {
-    const { fullName, email, phone, bloodGroup, gender, age, location } =
+    const { fullName, email, phone, bloodGroup, gender, age, location,parentId } =
       req.body;
 
     // Validate input
@@ -20,6 +20,7 @@ const donateBlood = async (req, res) => {
         .json({ success: false, message: "All fields are required" });
     }
     const donation = new Donation({
+      parentId,
       fullName,
       email,
       phone,
@@ -44,6 +45,36 @@ const donateBlood = async (req, res) => {
 
 //GET DONATOIN HISTORY
 const getDonorHistory = async (req, res) => {
+  try {
+    const { email } = req.params;
+
+    if (!email) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Email is required" });
+    }
+    const parentId = new mongoose.Types.ObjectId(email);
+    const donations = await Donation.find({ parentId});
+    console.log(donations)
+    if (!donations.length) {
+      return res
+        .status(404)
+        .json({ success: false, message: "No donation history found" });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Donar History Fetched Successfully",
+      donarData: donations,
+    });
+  } catch (error) {
+    console.log(error.message)
+    res
+      .status(500)
+      .json({ success: false, message: "Error fetching donar history" });
+  }
+};
+//GET DONATOIN HISTORY
+const getADonorHistory = async (req, res) => {
   try {
     const { email } = req.params;
 
--- backend/models/donation.js
+++ backend/models/donation.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 const donationSchema = new mongoose.Schema({
+  parentId:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users',
+  },
   fullName: { type: String, required: true },
   email: { type: String, required: true },
   phone: { type: Number, required: true },
