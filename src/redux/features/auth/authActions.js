957c5f9c95d840017caa8a16fb6b924d3521dcf8
Ajay <ajaykumar87577@gmail.com>
2025-02-26 16:34:35 +0530

    auth files completed

---
+++ src/redux/features/auth/authActions.js
@@ -0,0 +1,94 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import API from "../../../services/API";
+import { toast } from "react-toastify";
+import { Navigate } from "react-router-dom";
+
+export const userLogin = createAsyncThunk(
+  "auth/login",
+  async ({ role, email, password }, { rejectWithValue }) => {
+    try {
+      const { data } = await API.post("/auth/login", { role, email, password });
+      //store token
+      if (data.success) {
+        toast.success(data.message);
+        localStorage.setItem("token", data.token);
+        if (role === "admin") {
+          window.location.replace("/admin");
+        } else if (role === "donar") {
+          window.location.replace("/donar");
+        } else if (role === "hospital") {
+          window.location.replace("/home");
+        }
+      }
+      return data;
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        return rejectWithValue(error.response.data.message);
+      } else {
+        return rejectWithValue(error.message);
+      }
+    }
+  }
+);
+
+//register
+export const userSignUp = createAsyncThunk(
+  "auth/register",
+  async (
+    {
+      name,
+      role,
+      email,
+      password,
+      organisationName,
+      hospitalName,
+      address,
+      phone,
+    },
+    { rejectWithValue }
+  ) => {
+    try {
+      const { data } = await API.post("/auth/register", {
+        name,
+        role,
+        email,
+        password,
+        organisationName,
+        hospitalName,
+        address,
+        phone,
+      });
+      if (data.success) {
+        toast.success(data.message);
+        window.location.replace("/login");
+      }
+    } catch (error) {
+      console.log(error);
+      if (error.response && error.response.data.message) {
+        return rejectWithValue(error.response.data.message);
+      } else {
+        return rejectWithValue(error.message);
+      }
+    }
+  }
+);
+
+//current user
+export const getCurrentUser = createAsyncThunk(
+  "auth/getCurrentUser",
+  async ({ rejectWithValue }) => {
+    try {
+      const res = await API.get("/auth/current-user");
+      if (res.data) {
+        return res?.data;
+      }
+    } catch (error) {
+      console.log(error);
+      if (error.response && error.response.data.message) {
+        return rejectWithValue(error.response.data.message);
+      } else {
+        return rejectWithValue(error.message);
+      }
+    }
+  }
+);
