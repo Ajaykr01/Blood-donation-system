410f79d9c1930583cad193a797da5243e5143d95
Ajay <ajaykumar87577@gmail.com>
2025-02-26 16:09:14 +0530

    signUpPage completed

---
+++ src/auth/SignUp.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Form from "../components/shared/form/form";
+import { useSelector } from "react-redux";
+import Spinner from "../components/shared/Spinner";
+import { toast } from "react-toastify";
+
+const Signup = () => {
+  const { loading, error } = useSelector((state) => state.auth);
+  return (
+    <>
+      {error && <span>{toast.error(error)}</span>}
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className=" flex items-center justify-center bg-black">
+          <div className="w-full max-w-lg bg-white rounded-lg shadow-md p-8">
+            <Form
+              formTitle={"Create Your Account"}
+              submitBtn={"Sign Up"}
+              formType={"signUpType"}
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Signup;
