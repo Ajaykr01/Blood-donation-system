59a41b4579d03b34984d077407bfe4fa399fcbf6
Ajay <ajaykumar87577@gmail.com>
2025-02-27 14:44:29 +0530

    created delete donation history route

--- backend/routes/donationRoutes.js
+++ backend/routes/donationRoutes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const {
   donateBlood,
   getDonorHistory,
+  deleteDonationController,
 } = require("../controllers/donationController");
 const router = express.Router();
 
 router.post("/donate-blood", donateBlood);
 router.get("/donation-history/:email", getDonorHistory);
+router.delete("/delete-donation-history/:donationId", deleteDonationController);
 
 module.exports = router;
