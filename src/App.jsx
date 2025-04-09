99eb6064a321a80be91c31eba88c28167d0619c3
Ajay <ajaykumar87577@gmail.com>
2025-04-10 01:31:22 +0530

    imported some files and changed route path

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,7 @@ import Signup from "./auth/SignUp";
 import { ToastContainer } from "react-toastify";
 import ProtectedRoute from "./components/Routes/ProtectedRoute";
 import PublicRoute from "./components/Routes/PublicRoute";
-import Donar from "./pages/Dashboard/Donar";
-import Hospitals from "./pages/Dashboard/Hospitals";
-import Organisation from "./pages/Dashboard/Organisation";
-import Consumer from "./pages/Dashboard/Consumer";
+import Hospitals from "./pages/Hospital/Hospitals";
 import Donation from "./pages/Donation";
 import Analytics from "./pages/Dashboard/Analytics";
 import DonarList from "./pages/Admin/DonarList";
@@ -22,6 +19,14 @@ import AdminHome from "./pages/Admin/AdminHome";
 import HomePage from "./pages/HomePage";
 import DonationHistory from "./pages/Donar/DonationHistory";
 import DonorHome from "./pages/Donar/DonorHome";
+import PatientHome from "./pages/Patient/PatientHome";
+import RequestBlood from "./pages/Patient/RequestBlood";
+import RequestHistory from "./pages/Patient/RequestHistory";
+import PatientList from "./pages/Admin/PatientList";
+import Donations from "./pages/Admin/Donations";
+import BloodRequests from "./pages/Admin/BloodRequests";
+import HospitalDonations from "./pages/Hospital/HospitalDonations";
+import HospitalBloodRequests from "./pages/Hospital/HospitalBloodRequests";
 
 const App = () => {
   return (
@@ -37,7 +42,7 @@ const App = () => {
           }
         />
         <Route
-          path="/donar-list"
+          path="/admin/donar-list"
           element={
             <ProtectedRoute>
               <DonarList />
@@ -45,7 +50,15 @@ const App = () => {
           }
         />
         <Route
-          path="/hospital-list"
+          path="/admin/patient-list"
+          element={
+            <ProtectedRoute>
+              <PatientList />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/admin/hospital-list"
           element={
             <ProtectedRoute>
               <HospitalList />
@@ -53,10 +66,42 @@ const App = () => {
           }
         />
         <Route
-          path="/hospital"
+          path="/admin/donations"
+          element={
+            <ProtectedRoute>
+              <Donations />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/admin/blood-requests"
+          element={
+            <ProtectedRoute>
+              <BloodRequests />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/hospital-dashboard"
           element={
             <ProtectedRoute>
               <Hospitals />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/hospital/donations"
+          element={
+            <ProtectedRoute>
+              <HospitalDonations />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/hospital/blood-requests"
+          element={
+            <ProtectedRoute>
+              <HospitalBloodRequests />
             </ProtectedRoute>
           }
         />
@@ -69,34 +114,10 @@ const App = () => {
           }
         />
         <Route
-          path="/consumer"
-          element={
-            <ProtectedRoute>
-              <Consumer />
-            </ProtectedRoute>
-          }
-        />
-        <Route
           path="/donation"
           element={
             <ProtectedRoute>
               <Donation />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/organisation"
-          element={
-            <ProtectedRoute>
-              <Organisation />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/donar"
-          element={
-            <ProtectedRoute>
-              <Donar />
             </ProtectedRoute>
           }
         />
@@ -109,7 +130,7 @@ const App = () => {
           }
         />
         <Route
-          path="/donate-blood"
+          path="/donar/donate-blood"
           element={
             <ProtectedRoute>
               <DonateBlood />
@@ -117,10 +138,34 @@ const App = () => {
           }
         />
         <Route
-          path="/donation-history"
+          path="/donar/donation-history"
           element={
             <ProtectedRoute>
               <DonationHistory />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/patient-dashboard"
+          element={
+            <ProtectedRoute>
+              <PatientHome />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/patient/patient-request"
+          element={
+            <ProtectedRoute>
+              <RequestBlood />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/patient/patient-request-history"
+          element={
+            <ProtectedRoute>
+              <RequestHistory />
             </ProtectedRoute>
           }
         />
