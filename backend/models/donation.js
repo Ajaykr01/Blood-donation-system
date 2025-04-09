bcdbbe7e982f407684837e8723303befb6c84ac3
Ajay <ajaykumar87577@gmail.com>
2025-04-10 01:04:18 +0530

    did some changes in schema

--- backend/models/donation.js
+++ backend/models/donation.js
@@ -12,7 +12,13 @@ const donationSchema = new mongoose.Schema(
     bloodGroup: { type: String, required: true },
     gender: { type: String, required: true },
     age: { type: Number, required: true },
+    unit: { type: Number, required: true },
     location: { type: String, required: true },
+    status: {
+      type: String,
+      enum: ["Pending", "Approved", "Rejected"],
+      default: "Pending",
+    },
   },
   { timestamps: true }
 );
